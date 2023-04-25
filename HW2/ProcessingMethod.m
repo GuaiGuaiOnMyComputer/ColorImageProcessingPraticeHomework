65fa6e5c8adc60f8e3bce0b875882423f8728e2d
Eric <ericacccls@gmail.com>
2023-04-25 17:07:44 +0800

    extracted processing code from app class

--- HW2/ProcessingMethod.m
+++ HW2/ProcessingMethod.m
@@ -6,6 +6,39 @@ classdef ProcessingMethod
         Difference
         EdgeDetect
         HighPass
+        None
+    end
+
+    methods(Static, Access = public)
+
+        function result = MakeDarker(inputImg, ~)
+            result = inputImg - 64;
+        end
+
+        function result = MakeLRFlip(inputImg, ~)
+            result = fliplr(inputImg);
+        end
+
+        function result = GetTemporalDifference(currentFrame, previousFrame)
+            result = 50 * (currentFrame - previousFrame);
+        end
+
+        function result  = MakeNegative(inputImg, ~)
+            result = 255 - inputImg;
+        end
+
+        function result = DetectEdge(inputImg, ~)
+            result =  255*(repmat(uint8(edge(rgb2gray(inputImg), "sobel")), 1, 1, 3));
+        end
+
+        function result = GetHighPass(inputImg, ~)
+            result = imdilate(locallapfilt(inputImg, 0.5, 8), strel("square", 2));
+        end
+
+        function result = DoNotProcess(inputImg, ~)
+            result = inputImg;
+        end
+
     end
 end
 
