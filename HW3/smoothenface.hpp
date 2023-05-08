9b32be56afa290e93cfa041292cfdc001568345b
Eric <ericacccls@gmail.com>
2023-05-08 20:45:46 +0800

    fixed making-prof. face terrible bug

--- HW3/smoothenface.hpp
+++ HW3/smoothenface.hpp
@@ -13,11 +13,11 @@ public:
 public:
     L1(const cv::Mat &orig):CommonBase(orig)
     {
-        m_FaceMask = cv::Mat::zeros(m_ImgSize.height, m_ImgSize.width, CV_8UC3);
+        m_BlurredFace = cv::Mat::zeros(m_ImgSize.height, m_ImgSize.width, CV_8UC3);
         m_OriginalFace = cv::Mat::zeros(m_ImgSize.height, m_ImgSize.width, CV_8UC3);
         m_Background = cv::Mat::zeros(m_ImgSize.height, m_ImgSize.width, CV_8UC3);
         s_SmthResult = orig.clone();
-        m_MakeFaceMask();
+        m_MakeBlurredFace();
     }
 
 protected:
@@ -25,31 +25,33 @@ protected:
     {
         m_SmoothenBarPos = smthBarPos;
         m_Smoothness = (float)smthBarPos / 100;
-        cv::GaussianBlur(m_OriginalFace, s_SmthResult, cv::Size2d(13, 13), 100, 100);
+        cv::addWeighted(m_BlurredFace, m_Smoothness, m_OriginalFace, 1.0 - m_Smoothness, 0, s_SmthResult);
         s_SmthResult += m_Background;
-        cv::addWeighted(s_SmthResult, m_Smoothness, m_Original, 1.0 - m_Smoothness, 0, s_SmthResult);
     }
 
 private:
     int m_SmoothenBarPos;
 
 private:
-    void m_MakeFaceMask()
+    void m_MakeBlurredFace()
     {
-        cv::Mat imgHsv(m_ImgSize.height, m_ImgSize.width, CV_8UC3);
-        cv::cvtColor(m_Original, imgHsv, cv::COLOR_BGR2HSV);
-        cv::inRange(imgHsv, cv::Scalar(0, 30, 80), cv::Scalar(40, 255, 255), m_FaceMask);
-        m_FaceMask /= 255;
+        cv::Mat temp(m_ImgSize.height, m_ImgSize.width, CV_8UC3);
+        cv::Mat faceMask(m_ImgSize.height, m_ImgSize.width, CV_8UC1);
+        cv::cvtColor(m_Original, temp, cv::COLOR_BGR2HSV);
+        cv::inRange(temp, cv::Scalar(0, 30, 80), cv::Scalar(40, 255, 255), faceMask);
 
         cv::Mat erosionElement = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(15, 15));
-        cv::erode(m_FaceMask, m_FaceMask, erosionElement);
-        cv::copyTo(m_Original, m_OriginalFace, m_FaceMask);
-        cv::copyTo(m_Original, m_Background, 1 - m_FaceMask);
+        cv::erode(faceMask, faceMask, erosionElement);
+        m_Original.copyTo(m_OriginalFace, faceMask);
+        m_Original.copyTo(m_Background, cv::Scalar(255, 255, 255) - faceMask);
+        cv::cvtColor(temp, temp, cv::COLOR_HSV2BGR);
+        cv::GaussianBlur(m_Original, temp, cv::Size(13, 13), 100, 100);
+        temp.copyTo(m_BlurredFace, faceMask);
     }
 
 private:
     float m_Smoothness;
-    cv::Mat m_FaceMask;
+    cv::Mat m_BlurredFace;
     cv::Mat m_OriginalFace;
     cv::Mat m_Background;
 };
