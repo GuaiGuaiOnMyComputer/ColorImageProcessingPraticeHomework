999358ad6a0b67a72c01299f8e349651143805f1
Eric <ericacccls@gmail.com>
2023-06-04 13:25:50 +0800

    shapes of images to test

---
+++ FinalPresentation/experiment/imageshapes.hpp
@@ -0,0 +1,18 @@
+#ifdef _DEBUG
+#define TEST_COUNT 5000
+#else
+#define TEST_COUNT 20
+#endif
+
+struct ImageShape
+{
+    int width;
+    int height;
+};
+
+const ImageShape g_IMAGESHAPES[] = {
+        {800, 600},
+        {1024, 768},
+        {1280, 1024},
+        {1920, 1080}
+};
