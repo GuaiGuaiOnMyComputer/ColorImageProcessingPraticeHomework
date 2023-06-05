1089a218a1f201b3907308ac34ac41c2fb45c0b0
Eric <ericacccls@gmail.com>
2023-06-05 21:15:31 +0800

    bug fix

--- FinalPresentation/laplacianfilter.hpp
+++ FinalPresentation/laplacianfilter.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdint.h>
 
 namespace my
@@ -5,7 +7,7 @@ namespace my
     class LapFilter
     {
         public:
-        const int32_t LAP_FILTER[25] = {
+        static constexpr int32_t LAP_FILTER[25] = {
             0 ,  0, -1,  0, 0,
             0 , -1, -2, -1, 0,
             -1, -2, 16, -2, -1,
@@ -13,7 +15,7 @@ namespace my
             0 ,  0, -1,  0, 0,
         };
 
-        const int KER_SIZE = 5;
+        static const int KER_SIZE = 5;
 
         LapFilter() = delete;
     };
